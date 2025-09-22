29e67c64ac9ba1bdf4068bb1d8163a94b01614b5
ANSH KUMAR <137918915+ANSH514KUMAR@users.noreply.github.com>
2025-09-22 19:53:33 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-const menuBtn = document.getElementById('menu-btn');
-const sidePannel = document.getElementById("sidePannel");
-const mainOpacity = document.getElementById('mainContent');
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('active');
-    sidePannel.classList.toggle('active');
-    bodyOpacity.classList.toggle('active');
-});
