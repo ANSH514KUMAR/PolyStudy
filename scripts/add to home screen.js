ce3cfd1ce04ad8a854ee75658b6bc4e6a6d1d6ce
ANSH KUMAR <137918915+ANSH514KUMAR@users.noreply.github.com>
2025-10-26 12:32:40 +0530

    Update add to home screen.js

--- scripts/add to home screen.js
+++ scripts/add to home screen.js
@@ -1,11 +1,20 @@
-    let deferredPrompt;
-    const addBtn = document.querySelector("#addToHomeScreenButton");
-    
-    window.addEventListener("beforeinstallprompt", (e) => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-      // Update UI to notify the user they can add to home screen
-      addBtn.style.display = "block"; // Make the button visible
-    });
+    const installButton = document.getElementById('addToHomeScreenButton');
+    if (installButton) {
+      installButton.addEventListener('click', () => {
+        // Hide your custom UI
+        installButton.style.display = 'none';
+        
+        // Show the browser's A2HS prompt
+        deferredPrompt.prompt();
+        
+        // Track the user's choice
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null; // Clear the deferredPrompt
+        });
+      });
+    }
